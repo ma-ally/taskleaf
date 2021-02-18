486dc18dc44944762c86f38fadb0cd9fdf7d5f50
maejima <maejima4459@icloud com>
2021-02-18 11:31:11 +0900

    comment out login required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :info, :warning, :error
   helper_method :current_user
-  before_action :login_required
+  # before_action :login_required
 
   private
 
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def login_required
-    redirect_to login_path unless current_user
-  end
+  # def login_required
+  #   redirect_to login_path unless current_user
+  # end
 end
